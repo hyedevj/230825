1cec957595882702c94f1a26f1d8fe342ed5cc52
hyedevj <hyedevj@gmail.com>
2023-09-05 17:14:37 +0900

    useCallback 실습

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-// 1. 함수 컴포넌트는 자신의 상태가 변경될 때 리렌더링됨
-// 2. 부모 컴포넌트로부터 받는 prop이 변경될 때 리렌더링됨
-// 3. 부모 컴포넌트의 상태가 변경되면 리렌더링됨
-// React.memo
-
-import { useState } from "react";
-import Box from "./components/Box";
+import {useCallback, useState} from 'react'
+import Checkbox from "./components/Checkbox";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [foodOn, setFoodOn] = useState(false)
+	const [clothesOn, setClothesOn] = useState(false)
+	const [shelterOn, setShelterOn] = useState(false)
+
+	const foodChange = useCallback((e) => setFoodOn(e.target.checked), [])
+	const clothesChange = useCallback((e) => setClothesOn(e.target.checked), [])
+	const shelterChange = useCallback((e) => setShelterOn(e.target.checked), [])
 
 	return (
 		<div>
-			{count}
-			<button onClick={() => setCount(count + 1)}>+</button>
-			<Box />
+			<Checkbox label="Food" on={foodOn} onChange={foodChange} />
+			<Checkbox label="Clothes" on={clothesOn} onChange={clothesChange} />
+			<Checkbox label="Shelter" on={shelterOn} onChange={shelterChange} />
 		</div>
 	);
 }
