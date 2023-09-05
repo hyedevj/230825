dba0969f5fe987e6ecc1ba5c994e137e00693c83
hyedevj <hyedevj@gmail.com>
2023-09-05 17:00:06 +0900

    React.memo 실습

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-// useMemo
+// 1. 함수 컴포넌트는 자신의 상태가 변경될 때 리렌더링됨
+// 2. 부모 컴포넌트로부터 받는 prop이 변경될 때 리렌더링됨
+// 3. 부모 컴포넌트의 상태가 변경되면 리렌더링됨
+// React.memo
 
 import { useState } from "react";
-import ShowSum from "./components/ShowSum";
+import Box from "./components/Box";
 
 function App() {
-	const [label, setLabel] = useState("Result");
+	const [count, setCount] = useState(0);
 
 	return (
 		<div>
-			<button onClick={() => setLabel(label + ":")}>Change Label</button>
-			<ShowSum label={label} n={1000000000} />
+			{count}
+			<button onClick={() => setCount(count + 1)}>+</button>
+			<Box />
 		</div>
 	);
 }
