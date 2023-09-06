7f5f7950eedb5d114647b3466976ed527544f353
hyedevj <hyedevj@gmail.com>
2023-09-06 09:41:19 +0900

    Storybook 실습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import Box from "./components/Box";
-import Checkbox from "./components/Checkbox";
-import useHover from "./hooks/useHover";
-import useKeyPress from "./hooks/useKeyPress";
-import useToggle from "./hooks/useToggle";
 
 function App() {
-	const [on, toggle] = useToggle();
-	const [ref, isHover] = useHover();
-	const keyPressed = useKeyPress("a");
-
-	console.log(on);
-
-	return (
+		return (
 		<div>
-			<Checkbox checked={on} onChange={toggle} />
 			
-			{isHover ? 'hover' : 'mouseout'}
-			<Box ref={ref} />
-			
-			{keyPressed && "Pressed"}
 		</div>
 	);
 }
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,10 @@
-import styled from "@emotion/styled";
-
-const Box = styled.div`
-	width: 100px;
-	height: 100px;
-	background-color: blue;
-`;
+const Box = ({ width=100, height=100, backgroundColor="red" }) => {
+	const style = {
+		width,
+		height,
+		backgroundColor,
+	};
+	return <div style={style} />;
+};
 
 export default Box;
